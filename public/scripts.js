7a374338e97b8f218b1130816ec4a55913192e88
Zhaojie Tang <zhaojietang1997@gmail.com>
2019-09-05 19:52:33 -0500

    format

--- public/scripts.js
+++ public/scripts.js
@@ -3,83 +3,86 @@ let c = 0;
 let life = 7;
 let guessedWords = {};
 let currentInput = null;
+
 random();
-function random(){
-    fetch('/words')
-        .then(function(data) { 
-            data.text().then(function(text) {
-                word = text.toLowerCase();      
-                c = 0;
-                life = 7;
-                guessedWords = {};
-                document.getElementsByClassName('container')[0].innerHTML = '';
-                for(i = 0; i < word.length;i++){
-                    document.getElementsByClassName('container')[0].innerHTML += `<div class="character" id="input-${i+1}"></div>`;
-                }
-                document.getElementsByClassName('alphabet')[0].innerHTML = '';
-                fetch('/alphabet')
-                    .then(function(data) {
-                        data.json().then(function(alphabets){
-                            alphabets.forEach(function(key){
-                                document.getElementsByClassName('alphabet')[0].innerHTML += `<div class="alphabetkey"  onclick="keyClicked(this);"  data-key=${key}>${key.toUpperCase()}</div>`
-                            });
-                            currentInput = null;
-                            document.getElementById('life').innerHTML = life;
-                        });
-                    })
-                    .catch(function(error) {
-                        alert(error);
-                    });
-            });                  
-        })
-        .catch(function(error) {
+
+function random() {
+  fetch('/words')
+    .then(function (data) {
+      data.text().then(function (text) {
+        word = text.toLowerCase();
+        c = 0;
+        life = 7;
+        guessedWords = {};
+        document.getElementsByClassName('container')[0].innerHTML = '';
+        for (i = 0; i < word.length; i++) {
+          document.getElementsByClassName('container')[0].innerHTML += `<div class="character" id="input-${i + 1}"></div>`;
+        }
+        document.getElementsByClassName('alphabet')[0].innerHTML = '';
+        fetch('/alphabet')
+          .then(function (data) {
+            data.json().then(function (alphabets) {
+              alphabets.forEach(function (key) {
+                document.getElementsByClassName('alphabet')[0].innerHTML += `<div class="alphabetkey"  onclick="keyClicked(this);"  data-key=${key}>${key.toUpperCase()}</div>`
+              });
+              currentInput = null;
+              document.getElementById('life').innerHTML = life;
+            });
+          })
+          .catch(function (error) {
             alert(error);
-        });
-
-}
-function guess(){
-    document.getElementsByClassName('alphabet')[0].classList.add('non-click');
-    const guess = currentInput.toLowerCase();
-    if (guessedWords[guess]){
-        alert(`${guess} Already Used`);
-        document.getElementsByClassName('alphabet')[0].classList.remove('non-click');
-        return false;
-    } else {
-        guessedWords[guess] = true;
-    }
-
-    var url = '/guess';
-    var data = {character: guess};
-
-    fetch(url, {
-        method: 'POST', // or 'PUT'
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    }).then(res =>  res.json())
-    .then(response => {
-        document.getElementsByClassName('alphabet')[0].classList.remove('non-click');
-        let f = 0;
-        for (i = 0; i < response.length; i++){
-            if (response[i])
-            {
-                document.getElementById('input-' + (i+1)).innerHTML = guess;
-                c++;            
-                f = 1;
-            }
-        }
-        if (c === response.length) {alert('Success');random();}
-        if (f === 0){ life--; document.getElementById('life').innerHTML = life; }
-        if (life === 0) {alert('You are Dead');random();}
+          });
+      });
     })
-    .catch(error => console.error('Error:', error));   
+    .catch(function (error) {
+      alert(error);
+    });
 }
-function restart(){
-    random();
+
+function guess() {
+  document.getElementsByClassName('alphabet')[0].classList.add('non-click');
+  const guess = currentInput.toLowerCase();
+  if (guessedWords[guess]) {
+    alert(`${guess} Already Used`);
+    document.getElementsByClassName('alphabet')[0].classList.remove('non-click');
+    return false;
+  } else {
+    guessedWords[guess] = true;
+  }
+
+  var url = '/guess';
+  var data = { character: guess };
+
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(response => {
+      document.getElementsByClassName('alphabet')[0].classList.remove('non-click');
+      let f = 0;
+      for (i = 0; i < response.length; i++) {
+        if (response[i]) {
+          document.getElementById('input-' + (i + 1)).innerHTML = guess;
+          c++;
+          f = 1;
+        }
+      }
+      if (c === response.length) { alert('Success'); random(); }
+      if (f === 0) { life--; document.getElementById('life').innerHTML = life; }
+      if (life === 0) { alert('You are Dead'); random(); }
+    })
+    .catch(error => console.error('Error:', error));
 }
+
+function restart() {
+  random();
+}
+
 function keyClicked(event) {
-    currentInput = event.dataset.key;
-    event.classList.add('alphabetkey-disable');
-    guess();
+  currentInput = event.dataset.key;
+  event.classList.add('alphabetkey-disable');
+  guess();
 }
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const dictionary = ['hello','world','restaurant'];
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","h","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const dictionary = ['hello', 'world', 'restaurant'];
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "h", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -18,41 +18,41 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/words', (req, res) => {
-        let word = dictionary[Math.floor(Math.random()*dictionary.length)];
-        pickedWord = word;
-        res.send(word);
-    // let dictionary = [];
-    // client.connect(function(err) {
-    //     var db = client.db('hangman');
-    //     var collection = db.collection('words');
-    //     collection.find().toArray((err, items) => {
-    //         items.forEach(item => {
-    //             if (item != null) {
-    //                 dictionary.push(item.spell);
-    //             }
-    //         });
-    //         let word = dictionary[Math.floor(Math.random()*dictionary.length)];
-    //         pickedWord = word;
-    //         res.send(word);
-    //     })
-    // }); 
+  let word = dictionary[Math.floor(Math.random() * dictionary.length)];
+  pickedWord = word;
+  res.send(word);
+  // let dictionary = [];
+  // client.connect(function(err) {
+  //     var db = client.db('hangman');
+  //     var collection = db.collection('words');
+  //     collection.find().toArray((err, items) => {
+  //         items.forEach(item => {
+  //             if (item != null) {
+  //                 dictionary.push(item.spell);
+  //             }
+  //         });
+  //         let word = dictionary[Math.floor(Math.random()*dictionary.length)];
+  //         pickedWord = word;
+  //         res.send(word);
+  //     })
+  // }); 
 });
 
 app.get('/alphabet', (req, res) => {
-    res.send(alphabet);
-    // client.connect(function(err) {
-    //     var db = client.db('hangman');
-    //     var collection = db.collection('alphabet');
-    //     collection.findOne({},(err, item) => {
-    //         res.json(["a", "b", "c"]);
-    //     })
-    // }); 
+  res.send(alphabet);
+  // client.connect(function(err) {
+  //     var db = client.db('hangman');
+  //     var collection = db.collection('alphabet');
+  //     collection.findOne({},(err, item) => {
+  //         res.json(["a", "b", "c"]);
+  //     })
+  // }); 
 });
 
-app.post('/guess', function(req, res) {
-    let character = req.body.character;
-    const array = pickedWord.split('');
-    res.send(array.map(char => character == char));
+app.post('/guess', function (req, res) {
+  let character = req.body.character;
+  const array = pickedWord.split('');
+  res.send(array.map(char => character == char));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('Hangman app listening on port 3000!'));
